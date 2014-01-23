3a47ff3282a52b5997f86ba58368c9873b826e0d
Nate Wright <natewr@users.sourceforge.net>
2014-01-23 15:18:54 +0000

    * Move source into menu item div

--- fdm-templates/menu-item.php
+++ fdm-templates/menu-item.php
@@ -1,7 +1,7 @@
 <?php if ( $this->is_singular() ) : ?>
 <div id="<?php echo fdm_global_unique_id(); ?>" class="fdm-menu fdm-menu-item">
 <?php endif; ?>
-	
+
 	<?php echo $this->print_elements( 'header' ); ?>
 
 	<?php if ( $this->is_singular() ) : ?>
@@ -13,17 +13,18 @@
 		<div class="fdm-item-panel">
 
 			<?php echo $this->print_elements( 'body' ); ?>
-		
+
 			<div class="clearfix"></div>
 		</div>
+
+		<?php echo $this->print_elements( 'footer' ); ?>
 
 	<?php if ( $this->is_singular() ) : ?>
 	</div>
 	<?php else : ?>
 	</li>
 	<?php endif; ?>
-	
-	<?php echo $this->print_elements( 'footer' ); ?>
+
 
 <?php if ( $this->is_singular() ) : ?>
 </div>
